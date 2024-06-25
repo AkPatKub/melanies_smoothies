fdbf4be1857d7bb2e7df460e5d4ab460bd73b3e6
AkPatKub <akshaypathiyath@kubrickgroup.com>
2024-06-25 15:11:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
 
+pd_df=my_dataframe.to_pandas()
+
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients",
     my_dataframe,
